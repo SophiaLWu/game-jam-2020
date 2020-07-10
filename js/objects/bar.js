964a9ca6357a4595756e6973a14b3f607a22b981
Caleb Taylor <caleb89taylor@gmail.com>
2020-07-10 16:45:36 -0700

    add health bar

---
+++ js/objects/bar.js
@@ -0,0 +1,78 @@
+const MAX_VALUE = 100;
+const MIN_VALUE = 0;
+const MAX_DRAW = 96 / 100;
+
+/**
+ * Graphic for Bar that can be filled
+ * @extends Phaser.GameObjects.Graphics
+ */
+class Bar extends Phaser.GameObjects.Graphics {
+    /**
+     * Creates an instance of a Graphics game object
+     *
+     * @param {Object} params - constructor arguments for graphic class
+     * @param {Phaser.Scene} params.scene - scene the graphic belongs to
+     * @param {number} params.x - the horizontal position of graphic in the world
+     * @param {number} params.y - the horizontal position of graphic in the world
+     * @returns {Phaser.GameObjects.Graphics} - instance of graphic class
+     */
+    constructor({
+        scene, x, y, barWidth, barHeight, startValue
+    } = {}) {
+        super(scene);
+
+        this.x = x;
+        this.y = y;
+
+        this.barWidth = barWidth;
+        this.barHeight = barHeight;
+
+
+        if (startValue > MAX_VALUE || startValue < MIN_VALUE) {
+            throw new Error(`Invalid starting amount arg to Bar class. Must be within range: ${MIN_VALUE} - ${MAX_VALUE}`);
+        }
+        this.value = startValue;
+
+        this.draw();
+
+        scene.add.existing(this);
+        this.setActive(true);
+    }
+
+    draw() {
+        this.clear();
+
+        //  BG
+        this.fillStyle(0x000000);
+        this.fillRect(this.x, this.y, this.barWidth, this.barHeight);
+
+        //  Health
+        this.fillStyle(0xffffff);
+        this.fillRect(this.x + 2, this.y + 2, this.barWidth - 4, this.barHeight - 4);
+
+        if (this.value < 30) {
+            this.fillStyle(0xff0000);
+        }
+        else {
+            this.fillStyle(0x00ff00);
+        }
+
+        const fillWidth = Math.floor(MAX_DRAW * this.value);
+
+        this.fillRect(this.x + 2, this.y + 2, fillWidth, this.barHeight - 4);
+    }
+
+    update(amount) {
+        if (amount < MIN_VALUE) {
+            this.value = MIN_VALUE;
+        } else if (amount > MAX_VALUE) {
+            this.value = MAX_VALUE
+        } else {
+            this.value = amount;
+        }
+
+        this.draw();
+    }
+}
+
+export default Bar;
