3dd9a7da88fb7e4f89b3a958ae183f6a2cdd7042
Andrew Sweet <andrewmsweet@gmail.com>
2020-07-11 07:58:00 -0400

    Adding min spawn distances for objects

--- js/objects/ecosystem.js
+++ js/objects/ecosystem.js
@@ -1,3 +1,4 @@
+import { manhattanDistance } from "../utils.js"
 import { CONSTANTS } from "../constants.js"
 import Food from "./food.js";
 import Villager from "./villager.js";
@@ -20,9 +21,8 @@ class Ecosystem extends Phaser.GameObjects.Graphics {
     this.villagerBodies = this.physics.add.group();
     
     let {lastX, lastY} = {lastX: 0, lastY: 0};
-    const minDistance = 80;
     for (let i = 0; i < this.startingFoodAmount; i++) {
-      const {x, y} = this.getRandomSpawnLocation(lastX, lastY, minDistance);
+      const {x, y} = this.getRandomSpawnLocation(lastX, lastY);
       lastX = x;
       lastY = y;
       this.createFood(x,y);
@@ -31,7 +31,7 @@ class Ecosystem extends Phaser.GameObjects.Graphics {
     this.physics.add.collider(this.player.player, this.foodBodies, this.pickUpFood, null, this );
 
     for (let i = 0; i < this.startingVillagerAmount; i++){
-      const {x, y} = this.getRandomSpawnLocation(lastX, lastY, minDistance);
+      const {x, y} = this.getRandomSpawnLocation(lastX, lastY);
       lastX = x;
       lastY = y;
       this.createVillager(x, y);
@@ -76,12 +76,14 @@ class Ecosystem extends Phaser.GameObjects.Graphics {
     }.bind(this));
   }
 
-  getRandomSpawnLocation(farX, farY, minDistance) {
-    // Eventually randomly generate points around the globe
-    return {
-      x : Math.floor(Math.random() * 800),
-      y : Math.floor(Math.random() * 600),
-    };
+  getRandomSpawnLocation(farX, farY) {
+    let x = farX;
+    let y = farY;
+    while (manhattanDistance(x, y, farX, farY) < CONSTANTS.MIN_SPAWN_DISTANCE) {
+      x = Math.floor(Math.random() * 800);
+      y = Math.floor(Math.random() * 600);
+    }
+    return { x, y };
   }
 
   pickUpFood(player, foodBody) { //AndrewC: food here is physics body only, can't use methods from Food (or Collectible) classes.
@@ -90,12 +92,12 @@ class Ecosystem extends Phaser.GameObjects.Graphics {
     this.player.heal(1); // AndrewC: this shit only works because there's only one player
     this.player.eat();
 
-    const {x, y} = this.getRandomSpawnLocation();
+    const {x, y} = this.getRandomSpawnLocation(player.x, player.y);
     this.createFood(x, y);
   }
 
   villagerEatsFood(villager, foodBody) {
-    const {x, y} = this.getRandomSpawnLocation();
+    const {x, y} = this.getRandomSpawnLocation(villager.x, villager.y);
     this.createFood(x, y);
     foodBody.getFood().onCollision();
   }
