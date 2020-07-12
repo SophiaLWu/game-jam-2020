1d99c44498526236c48180f1cc58e99b45cdd803
Sophia Wu <sophia.luxi.wu@gmail.com>
2020-07-11 18:28:20 -0700

    Endgame sound

--- js/scenes/gameOverScene.js
+++ js/scenes/gameOverScene.js
@@ -15,6 +15,7 @@ class GameOverScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.audio('gameEndSound', '../../assets/sound/game_end.mp3');
   }
 
   create() {
@@ -23,6 +24,9 @@ class GameOverScene extends Phaser.Scene {
     this.add.text(30, CONSTANTS.SCREEN_HEIGHT / 3 + 74, `Food Eaten: ${this.foodEaten}`, { fontSize: '24px', fill: '#fff' });
     this.add.text(30, CONSTANTS.SCREEN_HEIGHT / 3 + 98, `Villagers Eaten: ${this.villagersEaten}`, { fontSize: '24px', fill: '#fff' });
     this.add.text(30, CONSTANTS.SCREEN_HEIGHT / 3 + 122, 'Press S to restart', { fontSize: '24px', fill: '#fff' });
+
+    let sfx = this.sound.add('gameEndSound', { volume: 0.4, loop: false });
+    sfx.play();
   }
 
   update() {
